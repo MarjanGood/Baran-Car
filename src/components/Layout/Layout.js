4b3b084816c6117dd790ed2feff8a41641394486
Goodarzyar <goodarzyar@ifb.ir>
2023-02-15 15:39:26 +0330

    session 29: Create layout

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,26 @@
+import styles from "./Layout.module.css";
+import Link from "next/link";
+
+function Layout({children}){
+
+    return(
+    <>
+    <header className={styles.header}>
+        <Link href="https://wwww.google.com" target="_blank">    
+         <h2>BARAN CAR</h2>
+         <p>Choose and buy your car</p>
+        </Link>
+    </header>
+
+    <div className={styles.container}>{children}</div>
+
+    <footer className={styles.footer}>
+      <a href="google.com" target="_blank">
+         BARAN 
+      </a>
+       Next.js courses | Baran Car Project &copy;
+    </footer>
+    </>
+    ) 
+}
+export default Layout
