53b1eb530cdd1c27424f0cc35a87a9a4c3e27a42
Goodarzyar <goodarzyar@ifb.ir>
2023-02-20 08:12:56 +0330

    session31create card components

--- src/components/templates/CarsPage.js
+++ src/components/templates/CarsPage.js
@@ -1,11 +1,14 @@
-import styles from "./CarsPage.module.css"
-function CarsPage({data}){
-    return(
-        <div className= {styles.container}>
-        {
-          data.map(car => <p key={car.id}>{car.name}</p>)
-        }
-        </div>
-    )
+import Card from "../module/Card";
+import styles from "./CarsPage.module.css";
+
+function CarsPage({ data }) {
+  return (
+    <div className={styles.container}>
+      {data.map((car) => (
+        <Card key={car.id} {...car} />
+      ))}
+    </div>
+  );
 }
-export default CarsPage 
+
+export default CarsPage;
