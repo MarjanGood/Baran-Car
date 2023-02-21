71e46057fa5b2528b5164f526fceee2ed7a520a6
Goodarzyar <goodarzyar@ifb.ir>
2023-02-21 11:07:14 +0330

    session38:add serachbar event and inputs with useState

--- src/pages/cars/index.js
+++ src/pages/cars/index.js
@@ -1,10 +1,12 @@
 import carsData from '../../data/carsData'
 import CarsPage from '../../components/templates/CarsPage';
 import Categories from '../../components/module/Categories';
+import SearchBar from '../../components/module/SearchBar';
 
 function Details(){
     return(
     <div>
+      <SearchBar></SearchBar>
       <Categories></Categories>
       <CarsPage data={carsData}></CarsPage>
     </div>
